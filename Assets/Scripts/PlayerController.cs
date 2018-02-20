df30adfa589b4113ffd2890958476bced2cf3903
Adam Dowding <467346@mymail.sussexdowns.ac.uk>
2018-02-20 11:43:08 +0000

    Fixed code but it's still broken

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,20 +6,25 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
-    private Rigidbody rb;
+    //private Rigidbody rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
+        //float moveHorizontal = 0;
+        //if (Input.GetKey("space"))
+        //{
+        //    moveHorizontal = speed;
+        //}
         float moveHorizontal = Input.GetAxis("Horizontal") * speed;
         float moveVertical = Input.GetAxis("Vertical") * speed;
 
 
-        transform.position = new Vector3(transform.position.x + (moveHorizontal), transform.position.y + (moveVertical), transform.position.z);
-
+        var newPos = new Vector3(transform.position.x + (moveHorizontal), transform.position.y + (moveVertical), transform.position.z);
+        transform.position = newPos;
     }
 }
